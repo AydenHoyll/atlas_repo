478b0e01f83aafbe12fbb725fa5396116cf91e25
ayden <aakazakov97@gmail.com>
2023-08-03 15:58:51 -0500

    small refactor for new feature

---
+++ src/components/list/index.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Checkbox, Input } from "antd";
+
+const ListComponent = ({ data, name }) => {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState(
+    data.map((key) => ({ key, checked: false }))
+  );
+
+  const filteredData = (items, input) => {
+    return items.filter((el) =>
+      el.key.toLowerCase().includes(input.toLowerCase())
+    );
+  };
+
+  const handleCheckboxChange = (itemKey) => {
+    setItems((prevItems) =>
+      prevItems.map((el) =>
+        el.key === itemKey ? { ...el, checked: !el.checked } : el
+      )
+    );
+  };
+  const getCheckedValues = (items) => {
+    return items.filter((item) => item.checked).map((item) => item.key);
+  };
+  const handleClear = () => {
+    setItems((prevItems) =>
+      prevItems.map((item) => ({
+        ...item,
+        checked: false,
+      }))
+    );
+  };
+
+  return (
+    <div className="w-1/2 mr-2 flex flex-col">
+      <Input
+        allowClear
+        placeHolder={`Search for ${name}`}
+        size="large"
+        bordered={false}
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      <button onClick={handleClear}>Clear All</button>
+      <div>
+        Checked {name}: {getCheckedValues(items).join(", ")}
+      </div>
+      <ul>
+        {filteredData(items, input).map((item) => (
+          <li className="" key={item.key}>
+            <Checkbox
+              className="mr-2"
+              checked={item.checked}
+              onClick={() => handleCheckboxChange(item.key)}
+            />
+            {item.key}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListComponent;
--- src/pages/customLexicon/index.js
+++ src/pages/customLexicon/index.js
@@ -1,118 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { LEXICON } from "../lexicon/LEXICON";
-import { Checkbox, Input } from "antd";
+import ListComponent from "../../components/list";
 
 const NewFeature = () => {
-  const [inputWord, setInputWord] = useState("");
-  const [inputLang, setInputLang] = useState("");
-
-  const wordKeys = Object.keys(LEXICON[0]).slice(
+  const wordData = Object.keys(LEXICON[0]).slice(
     9,
     Object.keys(LEXICON[0]).length
   );
-
-  const langVals = LEXICON.map((el) => Object.values(el)[0]);
-
-  // state for Words (i.e. LEXICON object keys)
-  const [checkedWordItems, setCheckedWordItems] = useState(
-    wordKeys.map((key) => ({ key, checked: false }))
-  );
-  // state for langs (i.e. LEXICON object[0] values)
-  const [checkedLangItems, setCheckedLangItems] = useState(
-    langVals.map((key) => ({ key, checked: false }))
-  );
-  const handleCheckboxChange = (item, items, Setter) => {
-    Setter(
-      items.map((el) =>
-        el.key === item ? { ...el, checked: !el.checked } : el
-      )
-    );
-  };
-
-  const getCheckedValues = (checkedItems) => {
-    return checkedItems.filter((item) => item.checked).map((item) => item.key);
-  };
-  const handleClear = (items, setter) => {
-    setter(
-      items.map((item) => ({
-        ...item,
-        checked: false,
-      }))
-    );
-  };
-  const filteredData = (data, input) => {
-    return data.filter((el) =>
-      el.key.toLowerCase().includes(input.toLowerCase())
-    );
-  };
+  const langData = LEXICON.map((el) => Object.values(el)[0]);
 
   return (
     <>
-      <div className="flex flex-row justify-evenly">
-        <div className="w-1/2 flex flex-col">
-          <button
-            onClick={() => handleClear(checkedWordItems, setCheckedWordItems)}
-          >
-            123
-          </button>
-          Checked values: {getCheckedValues(checkedWordItems).join(", ")}
-          <Input
-            allowClear={true}
-            size={"middle"}
-            value={inputWord}
-            onChange={(event) => setInputWord(event.target.value)}
-          />
-          <ul>
-            {filteredData(checkedWordItems, inputWord).map((item) => (
-              <li className="" key={item.key}>
-                <Checkbox
-                  className="mr-2"
-                  checked={item.checked}
-                  onClick={() =>
-                    handleCheckboxChange(
-                      item.key,
-                      checkedWordItems,
-                      setCheckedWordItems
-                    )
-                  }
-                />
-                {item.key}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="w-1/2 flex flex-col">
-          <button
-            onClick={() => handleClear(checkedLangItems, setCheckedLangItems)}
-          >
-            123
-          </button>
-          Checked values: {getCheckedValues(checkedLangItems).join(", ")}
-          <Input
-            allowClear={true}
-            size={"middle"}
-            value={inputLang}
-            onChange={(event) => setInputLang(event.target.value)}
-          />
-          <ul>
-            {filteredData(checkedLangItems, inputLang).map((item) => (
-              <li key={item.key}>
-                <Checkbox
-                  className="mr-2"
-                  checked={item.checked}
-                  onClick={() =>
-                    handleCheckboxChange(
-                      item.key,
-                      checkedLangItems,
-                      setCheckedLangItems
-                    )
-                  }
-                />
-                {item.key}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <button className="align-middle">Generate TABLE</button>
+      <div className="flex flex-row">
+        <ListComponent data={wordData} name="words" />
+        <ListComponent data={langData} name="languages" />
       </div>
     </>
   );
