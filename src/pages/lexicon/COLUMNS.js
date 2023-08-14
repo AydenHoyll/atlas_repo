75b805209c414ccac16768a4129f8d0de84ccbb3
ayden <aakazakov97@gmail.com>
2023-08-14 15:23:32 -0500

    added tentative colors obj for color marking, columns render logic adjusted

--- src/pages/lexicon/COLUMNS.js
+++ src/pages/lexicon/COLUMNS.js
@@ -1,15 +1,15 @@
+import { distinctiveColors } from "./utils";
+
 export const LEX_COL = (keys, color, number) => {
   const words = keys.map((word) => ({
     title: word,
     dataIndex: word,
-    render: (text) =>
-      number ? (
-        <span className={color ? "bg-amber-400" : ""}>
-          {text.word} {text.cognacy}
-        </span>
-      ) : (
-        <span className={color ? "bg-amber-400" : ""}>{text.word}</span>
-      ),
+    widths: "auto",
+    render: (text) => (
+      <span className={color ? distinctiveColors[text.cognacy] : ""}>
+        {text.word} {number && text.cognacy}
+      </span>
+    ),
   }));
   words.unshift({
     title: "Name",
--- src/pages/lexicon/utils.js
+++ src/pages/lexicon/utils.js
@@ -32,3 +32,44 @@ export const options = [
     value: "number",
   },
 ];
+// Mapping of distinctive colors to their conventional color names
+
+export const distinctiveColors = {
+  1: "bg-[#E6194B] font-semibold", // Red
+  2: "bg-[#3CB44B] text-white font-semibold", // Green
+  3: "bg-[#FFC20A] font-semibold", // Yellow
+  4: "bg-[#4363D8] text-white font-semibold", // Blue
+  5: "bg-[#F58231] text-white font-semibold", // Orange
+  6: "bg-[#911EB4] text-white font-semibold", // Purple
+  7: "bg-[#42D4F4] font-semibold", // Cyan
+  8: "bg-[#F032E6] text-white font-semibold", // Magenta
+  9: "bg-[#BFEF45] font-semibold", // Lime
+  10: "bg-[#DCBEFF] font-semibold", // Lavender
+  11: "bg-[#469990] text-white font-semibold", // Teal
+  12: "bg-[#9A6324] text-white font-semibold", // Brown
+  13: "bg-[#FFE119] font-semibold", // Light Yellow
+  14: "bg-[#AA6E28] text-white font-semibold", // Sienna
+  15: "bg-[#FFD8B1] font-semibold", // Peach
+  16: "bg-[#000075] text-white font-semibold", // Navy Blue
+  17: "bg-[#A9A9A9] font-semibold", // Dark Gray
+  18: "bg-[#800000] text-white font-semibold", // Maroon
+  19: "bg-[#808000] text-white font-semibold", // Olive
+  20: "bg-[#008080] text-white font-semibold", // Teal
+  21: "bg-[#000080] text-white font-semibold", // Navy
+  22: "bg-[#FF69B4] text-white font-semibold", // Hot Pink
+  23: "bg-[#8A2BE2] text-white font-semibold", // Blue Violet
+  24: "bg-[#7F00FF] text-white font-semibold", // Indigo
+  25: "bg-[#9400D3] text-white font-semibold", // Violet
+  26: "bg-[#6A5ACD] text-white font-semibold", // Slate Blue
+  27: "bg-[#32CD32] font-semibold", // Lime Green
+  28: "bg-[#00FF7F] font-semibold", // Spring Green
+  29: "bg-[#20B2AA] text-white font-semibold", // Light Sea Green
+  30: "bg-[#7FFF00] font-semibold", // Chartreuse
+  31: "bg-[#808080] font-semibold", // Gray
+  32: "bg-[#BDB76B] font-semibold", // Dark Khaki
+  33: "bg-[#8B4513] text-white font-semibold", // Saddle Brown
+  34: "bg-[#483D8B] text-white font-semibold", // Dark Slate Blue
+  35: "bg-[#2F4F4F] text-white font-semibold", // Dark Slate Gray
+  36: "bg-[#808000] text-white font-semibold", // Olive
+  37: "bg-[#000000] text-white", // Black
+};
